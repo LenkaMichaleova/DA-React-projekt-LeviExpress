ca42fc9072e35de3eb0599c55a0d9a94684699cb
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-17 15:17:06 +0200

    vytvoření komponenty pro zobrazení detailu cesty a zastávek.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
+import { JourneyDetail } from '../../components/JourneyDetail';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null)
@@ -12,7 +13,7 @@ export const HomePage = () => {
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange}/>
       {journey && 
-        <p>Nalezeno spojení s id {journey.journeyId}</p>
+        <JourneyDetail journey={journey}/>
       }
     </main>
   );
