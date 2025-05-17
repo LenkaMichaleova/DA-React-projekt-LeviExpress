a91afe02a251de892745244d8db942e707bdf3ae
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-17 16:25:34 +0200

    dokončení první části projektu - umožnění uživateli potvrdit rezervaci jízdenky

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,12 +2,32 @@ import { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
 import { SelectedSeat } from '../../components/SelectedSteat';
+import './style.css';
+import { useNavigate } from "react-router";
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null)
+  const navigate = useNavigate()
 
   const handleJourneyChange = (journeyData) => {
     setJourney(journeyData)
+  }
+
+  const handleBuy = async () => {
+    const response = await fetch("https://apps.kodim.cz/daweb/leviexpress/api/reservation", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: 'create',
+        seat: journey.autoSeat,
+        journeyId: journey.journeyId,
+      })
+    })
+    const data = await response.json()
+    const reservationId = data.results.reservationId
+    navigate(`/reservation/${reservationId}`)
   }
 
   return (
@@ -17,6 +37,12 @@ export const HomePage = () => {
         <>
           <JourneyDetail journey={journey}/>
           <SelectedSeat number={journey.autoSeat}/>
+
+          <div className="controls container">
+            <button className="btn btn--big" type="button" onClick={handleBuy}>
+              Rezervovat
+            </button>
+          </div>
         </>
       }
     </main>
