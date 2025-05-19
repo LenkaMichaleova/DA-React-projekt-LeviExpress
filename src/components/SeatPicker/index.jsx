fed85ccd9ee850c44361b0eb9212f07df8518744
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-19 22:08:40 +0200

    zobrazení vybraného sedadla na plánku autobusu

--- src/components/SeatPicker/index.jsx
+++ src/components/SeatPicker/index.jsx
@@ -1,13 +1,13 @@
 import { SeatRow } from '../SeatRow';
 import './style.css';
 
-export const SeatPicker = ({seats}) => {
+export const SeatPicker = ({seats, selectedSeat}) => {
   return (
     <div className="seat-picker container">
       <h2>Vyberte sedadlo</h2>
       <div className="seats">
         {seats.map((row, index) => (
-            <SeatRow key={index} row={row}/>
+            <SeatRow key={index} row={row} rowSelectedSeat={selectedSeat}/>
         ))}
       </div>
     </div>
